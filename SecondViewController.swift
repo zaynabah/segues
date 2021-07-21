a6427748c478c8bda5ef3a63c2eb9cfa1ce75424
Zaynab Ahmadi <ahmadizaynab7@gmail.com>
2021-07-21 10:58:13 -0700

    attempt 1

---
+++ SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  segues
+//
+//  Created by Tayyaba Ahmadi on 7/15/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBAction func startButton(_ sender: UIButton){
+    }
+    @IBOutlet weak var crButton: UIButton!
+    
+    @IBOutlet weak var lmButton: UIButton!
+    
+    @IBOutlet weak var nButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+        //hello
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ thirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  thirdViewController.swift
+//  segues
+//
+//  Created by Tayyaba Ahmadi on 7/16/21.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
